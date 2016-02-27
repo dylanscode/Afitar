3c59d82955ee0a2fc59365ccb66ea20d6a4f83fa
dcm51 <dylan.mcguire@pitt.edu>
2016-02-27 03:26:51 -0500

    connected to band

--- app/src/main/java/io/dylanmcguire/afitar/MainActivity.java
+++ app/src/main/java/io/dylanmcguire/afitar/MainActivity.java
@@ -1,6 +1,7 @@
 package io.dylanmcguire.afitar;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -16,11 +17,16 @@ import com.microsoft.band.BandException;
 import com.microsoft.band.BandInfo;
 import com.microsoft.band.ConnectionState;
 import com.microsoft.band.*;
+import com.microsoft.band.sensors.BandCaloriesEvent;
+
+import java.sql.Connection;
 
 public class MainActivity extends AppCompatActivity {
 
     public final static String EXTRA_MESSAGE = "io.dylanmcguire.afitar.MESSAGE";
     static String message = "not even close";
+    private TextView txtStatus;
+    private BandClient client = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        txtStatus = (TextView) findViewById(R.id.txtStatus);
+        new CheckConnectionTask().execute();
     }
 
     @Override
@@ -53,43 +62,80 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void checkConnection(View view) {
-        Intent intent = new Intent(this, ConfirmConnectionActivity.class);
+        final Intent intent = new Intent(this, ConfirmConnectionActivity.class);
 
         // connect to the band
         BandInfo[] pairedBands = BandClientManager.getInstance().getPairedBands();
-        final BandClient bandClient = BandClientManager.getInstance().create(this, pairedBands[0]);
+        BandClient bandClient = BandClientManager.getInstance().create(this, pairedBands[0]);
+        final BandPendingResult<ConnectionState> pendingResult = bandClient.connect();
 
         new Thread(new Runnable() {
 
             @Override
             public void run() {
-
-                TextView textView = new TextView(getApplicationContext());
-                textView.setTextSize(40);
-
-                // connect to the Band
-                BandPendingResult<ConnectionState> pendingResult = bandClient.connect();
-
                 try {
                     // check whether we're connected
                     ConnectionState state = pendingResult.await();
                     if (state == ConnectionState.CONNECTED) {
-                        textView.setText("connected!");
+                        message = "connected!";
                     } else {
-                        textView.setText("Not connected :(");
+                        message = "Not connected :(";
                     }
                 } catch (InterruptedException e) {
-                    textView.setText("interrupted");
+                    message = "interrupted";
                 } catch (BandException e) {
-                    textView.setText("band exception");
+                    message = "band exception";
                 }
 
-                LinearLayout layoutZZZ = (LinearLayout) findViewById(R.id.mcontentStuff);
-                layoutZZZ.addView(textView);
+                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
             }
         });
 
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+    }
+
+    // prints a message if necessary
+    private void appendToUI(final String str) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                txtStatus.setText(str);
+            }
+        });
+    }
+
+    // check if the band is connected
+    private boolean getConnectedBandClient() throws InterruptedException, BandException {
+        if (client == null) {
+            BandInfo[] devices = BandClientManager.getInstance().getPairedBands();
+            if (devices.length == 0) {
+                appendToUI("Band isn't paired with your phone.\n");
+                return false;
+            }
+            client = BandClientManager.getInstance().create(getBaseContext(), devices[0]);
+        } else if (ConnectionState.CONNECTED == client.getConnectionState()) {
+            return true;
+        }
+
+        appendToUI("Band is connecting...\n");
+        return ConnectionState.CONNECTED == client.connect().await();
+    }
+
+    private class CheckConnectionTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                if (getConnectedBandClient()) {
+                    appendToUI("Band is connected!");
+                    String fwVersion = client.getFirmwareVersion().await();
+                    appendToUI("Firmware version is " + fwVersion);
+                } else {
+                    appendToUI("Band is not connected. :(");
+                }
+            } catch (Exception e) {
+                appendToUI(e.getMessage());
+            }
+            return null;
+        }
     }
 }
